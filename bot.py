f0c37a4e1362134880db4604a19eb615bde94f7c
Hyeon Gu <68494132+QyuriLa@users.noreply.github.com>
2021-08-05 04:04:14 +0900

    수정 - 공식 예제 중 basic_bot.py 특화 및 적용
    Ref:
    https://github.com/Rapptz/discord.py/blob/v1.7.3/examples/basic_bot.py

--- bot.py
+++ bot.py
@@ -1,33 +1,81 @@
 import os
+import random
 import discord
+from discord.ext import commands
 import logging
 from dotenv import load_dotenv
 
-# Basic Logging Setup
-logger = logging.getLogger('discord')
-logger.setLevel(logging.DEBUG)
-handler = logging.FileHandler(filename='discord.log', encoding='utf-8', mode='w')
-handler.setFormatter(logging.Formatter('%(asctime)s:%(levelname)s:%(name)s: %(message)s'))
-logger.addHandler(handler)
+description = '''달에 가자...!'''
 
-# Initialize The Bot
-load_dotenv()
-TOKEN = os.getenv('DISCORD_TOKEN')
-
-client = discord.Client()
+bot = commands.Bot(command_prefix='유리! ', description=description)
 
 
-@client.event
+@bot.event
 async def on_ready():
-    print(f'We have logged in as {client.user}')
+    print('Logged in as')
+    print(bot.user.name)
+    print(bot.user.id)
+    print('------')
 
+@bot.command()
+async def add(ctx, left: int, right: int):
+    """Adds two numbers together."""
+    await ctx.send(left + right)
 
-@client.event
-async def on_message(message):
-    if message.author == client.user:
+@bot.command()
+async def roll(ctx, dice: str):
+    """Rolls a dice in NdN format."""
+    try:
+        rolls, limit = map(int, dice.split('d'))
+    except Exception:
+        await ctx.send('Format has to be in NdN!')
         return
 
-    if message.content.startswith('$hello'):
-        await message.channel.send('Hello!')
+    result = ', '.join(str(random.randint(1, limit)) for r in range(rolls))
+    await ctx.send(result)
 
-client.run(TOKEN)
+@bot.command(description='For when you wanna settle the score some other way')
+async def choose(ctx, *choices: str):
+    """Chooses between multiple choices."""
+    await ctx.send(random.choice(choices))
+
+@bot.command()
+async def repeat(ctx, times: int, content='repeating...'):
+    """Repeats a message multiple times."""
+    for i in range(times):
+        await ctx.send(content)
+
+@bot.command()
+async def joined(ctx, member: discord.Member):
+    """Says when a member joined."""
+    await ctx.send('{0.name} joined in {0.joined_at}'.format(member))
+
+@bot.group()
+async def cool(ctx):
+    """Says if a user is cool.
+    In reality this just checks if a subcommand is being invoked.
+    """
+    if ctx.invoked_subcommand is None:
+        await ctx.send('No, {0.subcommand_passed} is not cool'.format(ctx))
+
+@cool.command(name='릴라')
+async def _bot(ctx):
+    """Is the bot cool?"""
+    await ctx.send('Yes, the 릴라 is cool.')
+
+
+# 로그 Setup
+logger = logging.getLogger('discord')
+logger.setLevel(logging.DEBUG)
+handler = logging.FileHandler(
+    filename='discord.log', encoding='utf-8', mode='w'
+    )
+handler.setFormatter(
+    logging.Formatter('%(asctime)s:%(levelname)s:%(name)s: %(message)s')
+    )
+logger.addHandler(handler)
+
+# Token 로드 및 봇 실행
+load_dotenv()
+TOKEN = os.getenv('DISCORD_TOKEN')
+bot.run(TOKEN)
