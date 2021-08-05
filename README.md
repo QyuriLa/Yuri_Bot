8c72b2ed9bfb53f82055a4fc74d1c30cc990551d
Hyeon Gu <68494132+QyuriLa@users.noreply.github.com>
2021-08-05 15:04:14 +0900

    개선 - 커맨드를 Cog로 관리하도록 코드 수정 및 에러 핸들링 추가
    Ref:
    https://github.com/AlexFlipnote/discord_bot.py

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # Yuri_Bot
+
 개인용 디스코드 봇
+
+## Ref
+
+https://discordpy.readthedocs.io/en/latest/index.html#getting-started
+https://realpython.com/how-to-make-a-discord-bot-python/
+https://github.com/AlexFlipnote/discord_bot.py/blob/master/index.py
--- bot.py
+++ bot.py
@@ -1,79 +1,38 @@
 import os
-import random
+import logging
+
 import discord
 from discord.ext import commands
-import logging
 from dotenv import load_dotenv
 
-description = '''달에 가자...!'''
 
-bot = commands.Bot(command_prefix='유리! ', description=description)
-
-
-@bot.event
-async def on_ready():
-    print('Logged in as')
-    print(bot.user.name)
-    print(bot.user.id)
-    print('------')
-
-@bot.command()
-async def add(ctx, left: int, right: int):
-    """Adds two numbers together."""
-    await ctx.send(left + right)
-
-@bot.command()
-async def roll(ctx, dice: str):
-    """Rolls a dice in NdN format."""
-    try:
-        rolls, limit = map(int, dice.split('d'))
-    except Exception:
-        await ctx.send('Format has to be in NdN!')
-        return
-
-    result = ', '.join(str(random.randint(1, limit)) for r in range(rolls))
-    await ctx.send(result)
-
-@bot.command(description='For when you wanna settle the score some other way')
-async def choose(ctx, *choices: str):
-    """Chooses between multiple choices."""
-    await ctx.send(random.choice(choices))
-
-@bot.command()
-async def repeat(ctx, times: int, content='repeating...'):
-    """Repeats a message multiple times."""
-    for i in range(times):
-        await ctx.send(content)
-
-@bot.command()
-async def joined(ctx, member: discord.Member):
-    """Says when a member joined."""
-    await ctx.send('{0.name} joined in {0.joined_at}'.format(member))
-
-@bot.group()
-async def cool(ctx):
-    """Says if a user is cool.
-    In reality this just checks if a subcommand is being invoked.
-    """
-    if ctx.invoked_subcommand is None:
-        await ctx.send('No, {0.subcommand_passed} is not cool'.format(ctx))
-
-@cool.command(name='릴라')
-async def _bot(ctx):
-    """Is the bot cool?"""
-    await ctx.send('Yes, the 릴라 is cool.')
-
+# Bot kwargs 초기화
+bot = commands.Bot(
+    command_prefix='유리! ',
+    description='Prefix: "유리! "',
+    owner_id=226671028460322818,
+    allowed_mentions=discord.AllowedMentions(
+        everyone=False, users=True, roles=False, replied_user=True
+    ),
+    intents=discord.Intents.all()
+)
 
 # 로그 Setup
 logger = logging.getLogger('discord')
 logger.setLevel(logging.DEBUG)
 handler = logging.FileHandler(
     filename='discord.log', encoding='utf-8', mode='w'
-    )
+)
 handler.setFormatter(
     logging.Formatter('%(asctime)s:%(levelname)s:%(name)s: %(message)s')
-    )
+)
 logger.addHandler(handler)
+
+# Cogs 로드
+for file in os.listdir("cogs"):
+    if file.endswith(".py"):
+        name = file[:-3]
+        bot.load_extension(f"cogs.{name}")
 
 # Token 로드 및 봇 실행
 load_dotenv()
