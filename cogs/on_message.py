53f3382fdfa55b6527c872039c7882c2291ba7cb
Hyeon Gu <68494132+QyuriLa@users.noreply.github.com>
2021-08-10 03:06:03 +0900

    :recycle: 리펙터링 - config를 on_message로 옮겨 함수 인자에서 제외

--- cogs/on_message.py
+++ cogs/on_message.py
@@ -1,18 +1,23 @@
 import discord
 from discord.ext import commands
 
-from utils import default
-from bot import bot as bot_
+from utils import default, pingpong
 
 
 class OnMessage(commands.Cog):
     def __init__(self, bot):
         self.config = default.config()
+        self.bot = bot
 
     @commands.Cog.listener()
     async def on_message(self, message):
+        if not isinstance(message.channel, discord.TextChannel):
+            return
+
         await react_to_chito(message)
-        await listen_to_bot(message, self.config)
+
+        if message.author.id in self.config["bot_whitelist"]:
+            await listen_to_bot(message, self.bot)
 
 
 async def react_to_chito(message):
@@ -22,11 +27,9 @@ async def react_to_chito(message):
             await message.add_reaction('❤')
 
 
-async def listen_to_bot(message, config):
-    bot_whitelist = config["bot_whitelist"]
-    if message.author.id in bot_whitelist:
-        ctx = await bot_.get_context(message)
-        await bot_.invoke(ctx)
+async def listen_to_bot(message, bot):
+    ctx = await bot.get_context(message)
+    await bot.invoke(ctx)
 
 
 def setup(bot):
