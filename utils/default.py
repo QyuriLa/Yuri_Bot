9c166a1b59a3a5aad4497b65a4b375505129086d
Hyeon Gu <68494132+QyuriLa@users.noreply.github.com>
2021-08-17 23:05:27 +0900

    :wrench: 추가 - config.json

--- utils/default.py
+++ utils/default.py
@@ -10,7 +10,7 @@ from io import BytesIO
 def config(filename: str = "config"):
     """ Fetch default config file """
     try:
-        with open(f"{filename}.json", encoding='utf8') as data:
+        with open(f"data/{filename}.json", encoding='utf8') as data:
             return json.load(data)
     except FileNotFoundError:
         raise FileNotFoundError("JSON file wasn't found")
